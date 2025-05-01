a54f152b30b0195b5e13bf7b6f55f14c12a8d6b4
alexander-dybdahl <alexldybdahl@gmail.com>
2025-05-01 21:41:37 +0200

    Remove deprecated files and implement core components for FBSNN model with Sine activation function

---
+++ core/activation_functions.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class Sine(nn.Module):
+    """This class defines the sine activation function as a nn.Module"""
+    def __init__(self):
+        super(Sine, self).__init__()
+
+    def forward(self, x):
+        return torch.sin(x)
